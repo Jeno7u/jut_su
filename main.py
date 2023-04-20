bd63f6651f77cf024ca07cebc1ed15855b3e37bf
Jeno7u <114745021+Jeno7u@users.noreply.github.com>
2023-04-20 10:01:46 +0300

    created interface

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from printing import print_info
-from download import download_video
+import sys
+from PyQt5.QtWidgets import QApplication
+from init_gui import Window
 
 
 def main():
-    print_info()
-
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+    
 
 if __name__ == '__main__':
     main()
